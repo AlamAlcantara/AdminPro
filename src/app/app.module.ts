5ace4c0398898170923c82e696985722705bc27b
alamalcantara <alamalcantara99@gmail.com>
2019-10-14 18:37:58 -0400

    register form partially handled

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 //Modulos
 import { PagesModule } from './pages/pages.module';
@@ -27,6 +27,7 @@ import { RegisterComponent } from './login/register.component';
     AppRoutingModule,
     PagesModule,
     FormsModule,
+    ReactiveFormsModule,
     ChartsModule,
     ServiceModule
   ],
